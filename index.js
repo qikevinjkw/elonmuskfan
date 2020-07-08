d6de48c888eb485f4cac3a7b770d99f97e6ae4f3
kevin qi <kevinqi@kevins-MBP-2.fios-router.home>
2020-07-08 15:58:16 -0400

    Remove secrets

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const Twitter = require('twitter');
 const twitter = new Twitter({
-  consumer_key: '30GGTW4feV1NEDknlHvtg2f8o',
-  consumer_secret: 'iMLXTuBnTkrwaSNcfZOm1emaMTrygzyARPrKQXkL8mfSl9M3Rw',
-  access_token_key: '1280910845246164994-SJSOGNrBpyBBe8xm2EqlbNySI5VtBR',
-  access_token_secret: 'tCIv4dMarVdMhnj8VOhSsKdXmsSdVpwZCog6RqXqmf7v9'
+  consumer_key: process.env.API_KEY, // Declared in heroku env, heroku config:get API_KEY
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS_KEY,
+  access_token_secret: process.env.ACCESS_SECRET,
 });
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.get('/test', (req, res) => {
+    res.json({
+        message: "It works!"
+    })
+})
 app.listen(PORT, () => {
 let day = 1;
 
@@ -26,25 +31,3 @@ setInterval(() => {
 
 console.log(`App listening on port ${PORT}`)
 });
-
-// twitter.get('statuses/user_timeline', {screen_name: 'epic_qi'}, (err, data, resp) => {
-//     if (!err) {
-//         console.log("Verified account", data)
-//     } else {
-//         console.log("Error getting tweet", err)
-//     }
-// })
-
-
-
-/**
- * API Key: IOdiYoeaHlwSaGusO9fvrPtVJ
- * 
- * API Secret key: Xmquqq5XbYXpNwfeSqIDw0gcAENIj9VnOila3eIibNhloIaCCw
- * 
- * Bearer token: AAAAAAAAAAAAAAAAAAAAAFFQFwEAAAAAyZbHegzrQoGB%2BrT3R9oZdOc90MI%3Db55tAuQ2ERE8PRgeYAKcdw07HtiW4K4GFvVpTGGAwsSojeOily
- * 
- * Access token: 1280910845246164994-xyUnpGKWeoO3FxQ0RsfIkje5LO6azK
- * 
- * Access token secret: 6x2TZgnuMUrxnhSGjbTNIIgB7KX8nxcCit2hNFs0Mf2Ve
- */
